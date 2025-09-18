38c8fbadafed8bf5f5654f02c3f66d0610caa3d4
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-09-18 07:56:03 +0000

    correzione

---
+++ informatica/2025_09_18_01.c
@@ -0,0 +1,6 @@
+/*Scrivere un programma in c che gestisca un insieme di numeri interi usando un array dinamico.
+Il programma deve permettere, tramite menù:
+1. Aggiungere un numero (espandendo l'array con realloc)
+2. Visualizzare tutti i numeri
+3. Cercare un numero specifico
+4. */
