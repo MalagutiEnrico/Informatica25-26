11b929717199cbb2deb6a7115f3de82d442b8705
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-09-25 09:12:02 +0000

    correzione esercizio

--- informatica/2025_09_20compito1.c
+++ informatica/2025_09_20compito1.c
@@ -11,24 +11,23 @@ Il programma deve permettere, tramite menù:
 #include <stdlib.h>
 #include "Arraylib.h"
 #include "Arraylib.c"
-#define DIMiniziale 5
 
 void stampaMenu(int *scelta){
-    system("CLS");
+    system("clear");
     printf("*****Gestione di un insieme di numeri utilizzando un array dinamico*****\n");
     printf("1.Aggiungi un numero al vettore\n2.Visualizza tutti i numeri\n3.Cerca un numero\n");
     printf("4.Ordina i numeri in ordine crescente\n5.Elimina un numero\n6.Esci dal programma.\n");
-    printf("Effettua una scelta:\n");
+    printf("Effettua una scelta:");
     scanf("%d", &*scelta);
 }
 
 void aggiungiElemento(int vett[], int *dim){
     (*dim)++;
-    int *temp = (int *)realloc(vett, (*dim)*sizeof(int));
-    if(temp != NULL)    
-        *vett = *temp;
-    else 
+    vett = (int *)realloc(vett, (*dim)*sizeof(int));
+    if(vett == NULL){
+        printf("Errore nell'allocazione della memoria\n");
         (*dim)--;
+    }
 }
 
 int trovaNumero(int vett[], int dim, int num){
@@ -44,17 +43,17 @@ void eliminaElemento(int vett[], int *dim, int num){
         vett[i] = vett[i+1];
     }
     (*dim)--;
-    int *temp = (int *)realloc(vett, (*dim)*sizeof(int));
-    if(temp != NULL)
-        *vett = *temp;
-    else
+    vett = (int *)realloc(vett, (*dim)*sizeof(int));
+    if(vett == NULL){
+        printf("Errore nell'allocazione della memoria.\n");
         (*dim)++;
+    }
 }
 
 int main(){
-    int *vettore, n=1, dim=DIMiniziale, num, scelta, pos, numdaeliminare;
+    int *vettore = NULL;    //array dinamico
+    int n=1, dim=0, num, scelta, pos;
     vettore = (int *)malloc(dim*sizeof(int));
-    riempiVettoreCasuale(vettore, dim, 0, 100);
     do{
         stampaMenu(&scelta);
         switch(scelta){
@@ -64,8 +63,14 @@ int main(){
                 scanf("%d", &vettore[dim-1]);
                 break;
             case 2:
-                printf("Il vettore stampato: ");
-                stampaVettore(vettore, dim, ' ');
+                if(dim==0){
+                    printf("La dimensione del vettore è 0. Non ci sono elementi da stampare.\n");
+                }
+                else{
+                    printf("Il vettore stampato: ");
+                    stampaVettore(vettore, dim, ' ');
+                    printf("\n");
+                }
                 break;
             case 3:
                 printf("Inserisci l'elemento da cercare: ");
@@ -83,14 +88,14 @@ int main(){
                 break;
             case 5:
                 printf("Inserisci il numero da eliminare: ");
-                scanf("%d", &numdaeliminare);
-                eliminaElemento(vettore, &dim, numdaeliminare);
+                scanf("%d", &num);
+                eliminaElemento(vettore, &dim, num);
                 break;
             case 6:
                 printf("Il programma è terminato.\n");
                 break;
         }
-        system("PAUSE");
+        pausa();
     }while(scelta!=6);
     free(vettore);
     return 0;
