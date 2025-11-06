6cf4a99c474d63c457b2ff175d752a7be738ad26
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-11-06 12:57:25 +0000

    esercizio codice fiscale

--- informatica/2025_11_04_CODICE_FISCALE.c
+++ informatica/2025_11_04_CODICE_FISCALE.c
@@ -8,48 +8,46 @@ typedef char* String;
 
 #define DIM 16
 #define DATA 11
+#define DIMANAGRAFICA 2
+#define DIMDATA 3
 
-void compatta(String str, int len){
+void compatta(String str, int len){     //compatta la stringa per eliminare gli spazi vuoti
     if(len>0 && str[len-1]=='\n'){
         str[len-1] = '\0';
         len--;
     }
 }
 
-Bool isVocale(char c){
+Bool isVocale(char c){                  //controlla se la lettera è una vocale
     if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')        return true;        //controllo se vocale minuscola
     else if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')   return true;        //controllo se vocale maiuscola
     else                                                                return false;       //non è vocale, ritorna false
 }
 
-void shiftDx(int *vett, char c){
-    int i=0;
-    while(vett[i]!='\0'){
-        vett[i] = vett[i+1];
-        i++;
-    }
+void shiftDX(String str, int pos){
+    int i=pos;
+    while(str[i]!='\0')     str[i++] = str[i+1];
 }
 
-String calcoloNomi(String str){
+String calcoloNomi(String str){         //calcola i tre caratteri del nome e del cognome da inserire nel codce fiscale
     int pos=0, cnt=0;
-    String tmp = malloc(4*sizeof(char));
+    String tmp = malloc(4*sizeof(char));                                        //crea la stringa per contenere i caratteri
     if(tmp==NULL)   exit(1);
     while(str[pos]!='\0'){
-        if(!isVocale(str[pos]) && cnt<3)         tmp[cnt++] = str[pos];
+        if(!isVocale(str[pos]) && cnt<3)         tmp[cnt++] = str[pos];         //se incontra una vocale e ha isnerito meno di 3 caratteri, allora inserisci il carattere
         pos++;
     }
-    if(cnt<3){
+    if(cnt<3){                                                                  //se il contatore è minore di tre allora inserisci le vocali mancanti
         pos=0;
         while(cnt<3){
-            if(isVocale(str[pos]))             tmp[cnt++] = str[pos];
+            if(isVocale(str[pos]))             tmp[cnt++] = str[pos++];
         }
-        pos++;
     }
     return tmp;
 }
 
 void calcolaAnagrafica(String* matrix){
-    for(int i=0; i<2; i++){
+    for(int i=0; i<DIMANAGRAFICA; i++){
         if(i==0)    printf("Inserisci il nome: ");
         else        printf("Inserisci il cognome: ");
         matrix[i] = creaStringa('\n');
@@ -58,29 +56,48 @@ void calcolaAnagrafica(String* matrix){
     }
 }
 
-void calcolaData(String *matrix){
-    String dataNascita;
-    int pos=2;
+char calcoloMese(String str){
+    char vettore[] = {'A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T'};
+    int num = atoi(str);
+    return vettore[num];
+}
+
+char calcoloGiorno(String str, char c){
+    if(c == 'f'){
+        int num = atoi(str);
+        num += 40;
+        sprintf(str, "%d", num);
+    }
+    return str;
+}
+
+void calcolaData(String *matrix, char c){
+    char dataNascita[15];
     printf("Inserisci la data di nascita (formato gg/mm/aa): ");
-    dataNascita = creaStringa('\n');
-    for(int i=0; i<3; i++){
-        matrix[i] = malloc(3*sizeof(char));
-        matrix[i] = strtok(dataNascita, "/");
-        shiftDx(matrix[i], '/');
-        pos+=2;
+    fgets(dataNascita, 15, stdin);
+    for(int i=0; i<DIMDATA; i++){
+        matrix[i] = malloc(DIMDATA*sizeof(char));
+        matrix[i] = strtok(dataNascita, "/\0");
         printf("%s\n", matrix[i]);
     }
-    
+    //matrix[1] = calcoloMese(matrix[1]);
+    //matrix[2] = calcoloGiorno(matrix[2], c);
+}
+
+String compattaCodice(char dest, String* strNomi, String* strData){
+
 }
 
 int main(){
     char codiceFiscale[DIM+1], genere;
     String *anagrafica = malloc(2*sizeof(String));
-    String *dataNascitaSep = (String)malloc(3*sizeof(char));
+    String *dataNascitaSep = (String)malloc(DIMDATA*sizeof(char));
     printf("*****Calcolo Codice Fiscale*****\n");
     calcolaAnagrafica(anagrafica);
-    calcolaData(dataNascitaSep);
-    printf("Inserisci il genere: ");
+    printf("Inserisci il genere(m-maschio, f-femmina): ");
     scanf(" %c", &genere);
+    calcolaData(dataNascitaSep, genere);
+    compattaCodice(codiceFiscale, anagrafica, dataNascitaSep);
+    printf("Il codice fiscale stampato: %s\n", codiceFiscale);
     return 0;
 }
